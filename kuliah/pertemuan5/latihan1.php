8dbd3578c52fa6ec3d6cee79c62e48b3afdd0bf3
NajranAlFaresy <najranalfarsy06@gmail.com>
2024-03-10 17:22:39 +0700

    comimit tugas 4

---
+++ kuliah/pertemuan5/latihan1.php
@@ -0,0 +1,10 @@
+<?php
+
+// 1. membuat Array
+$hari = array("Senin", "Selasa", "Rabu");
+$bulan = ["Januari", "Februari", "Maret"];
+$mahasiswa = ["Najran", 2.5, false];
+
+// 2. Mencetak isi Array
+// Mencetak 1 elemen pada array, menggunakan index
+ 
